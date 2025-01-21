9ba4189a0d4f40e611dd6b7f499e2db2b1144b73
Tomasi - Developing <info@tomasi-developing.ch>
2025-01-21 12:56:49 +0100

    fix week pipe bug

--- Ui/src/app/helpers/week.pipe.ts
+++ Ui/src/app/helpers/week.pipe.ts
@@ -1,4 +1,5 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
+import moment from "moment";
 
 @Pipe({
   name: 'week'
@@ -6,11 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class WeekPipe implements PipeTransform {
 
   transform(value: Date, ...args: unknown[]): unknown {
-    const d = new Date(value);
-    let yearStart = +new Date(d.getFullYear(), 0, 1);
-    let today = +new Date(d.getFullYear(),d.getMonth(),d.getDate());
-    let dayOfYear = ((today - yearStart + 1) / 86400000);
-    return Math.ceil(dayOfYear / 7);
+    return moment(value).isoWeek();
   }
-
 }
